a2a6f88d63569fa28db82bec94cef271e2ebea49
Amritpal Singh <amritpal.am26@gmail.com>
2021-05-29 17:37:34 -0700

    create code tree from frequency table

--- HuffTreeNode.java
+++ HuffTreeNode.java
@@ -1,20 +1,21 @@
+import java.util.PriorityQueue;
 public class HuffTreeNode implements Comparable<HuffTreeNode> {
 
-    public int val;
+    public int symbol;
     public int weight;         // Number of times the character occcurs
     public HuffTreeNode left;
     public HuffTreeNode right;
 
-    public HuffTreeNode(int val, int weight, HuffTreeNode leftChild, HuffTreeNode rightChild) {
-        this.val = val;
+    public HuffTreeNode(int symbol, int weight, HuffTreeNode leftChild, HuffTreeNode rightChild) {
+        this.symbol = symbol;
         this.weight = weight;
         this.left = leftChild;
         this.right = rightChild;
     }
 
-    public HuffTreeNode(int val) {
-        this.val = val;
-        this.weight = 0;
+    public HuffTreeNode(int symbol, int weight) {
+        this.symbol = symbol;
+        this.weight = weight;
         this.left = null;
         this.right = null;
     }
@@ -23,4 +24,30 @@ public class HuffTreeNode implements Comparable<HuffTreeNode> {
     public int compareTo(HuffTreeNode node) {
         return weight - node.weight;
     }
+
+    public static HuffTreeNode rootFromFrequencyTable(FrequencyTable table) throws RuntimeException {
+        if (table.isEmpty()) {
+            throw new RuntimeException("Cannot create table code tree from empty table.");
+        }
+
+        PriorityQueue<HuffTreeNode> queue = new PriorityQueue<HuffTreeNode>();
+        for(int i = 0; i < table.getSize(); i++) {
+            if (table.getFrequency(i) > 0) {
+                queue.add(new HuffTreeNode(i, table.getFrequency(i)));
+            }
+        }
+
+        System.out.println(queue.size());
+        while (queue.size() > 1) {
+            HuffTreeNode left = queue.remove();
+            HuffTreeNode right = queue.remove();
+            HuffTreeNode newNode = new HuffTreeNode(-1, left.weight+right.weight, left, right);
+            queue.add(newNode);
+        }
+
+        HuffTreeNode root = queue.remove();
+        System.out.println("weight of root: " + root.weight);
+        return root;
+    }
+
 }
